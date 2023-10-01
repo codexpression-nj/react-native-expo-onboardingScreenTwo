60d9f02afea4a5cd3102844d9d99bc3b935e680a
codexpression-nj <n.codexpression@gmail.com>
2023-10-01 11:07:02 +0200

    update

---
+++ constants/theme.js
@@ -0,0 +1,17 @@
+const COLORS = {
+    primary: "#836629",
+    secondary: "#555555",
+    tertiary: "#FF7754",
+
+    slideone:'#D7D45C',
+    slideThree:'#D9DFE9',
+    slideTwo:'#F8D25D',
+    slideFour:'#BEBEBD',
+  
+    gray: "#83829A",
+    gray2: "#C1C0C8",
+  
+    white: "#F3F4F8",
+    lightWhite: "#FAFAFC",
+  };
+  export { COLORS };
---
+++ data/sldes.js
@@ -0,0 +1,19 @@
+import { COLORS } from '../contants/theme';
+
+export default [
+    {
+        id: 1,
+        title: '01.',
+        subTitle:'Stylish Vase',
+        description: 'Elegant vase for floral arrangements',
+        image: require('../assets/avo.png'),
+        backgroundColor:COLORS.slideone
+    },    {
+        id:2,
+        title: '02.',
+        subTitle:'Morden Chair',
+        description: 'Comfortable armchair with modern design',
+        image: require('../assets/banana.png'),
+        backgroundColor:COLORS.slideTwo
+    },   
+]
