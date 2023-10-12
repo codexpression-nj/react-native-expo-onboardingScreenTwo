0afb5dc79e50157913fbb02de3882854400cedf2
codexpression-nj <n.codexpression@gmail.com>
2023-10-12 23:29:35 +0200

    update

--- pages/onBoardingItem.jsx
+++ pages/onBoardingItem.jsx
@@ -5,9 +5,14 @@ import { View, Text, StyleSheet } from 'react-native';
 // create a component
 const OnBoardingItem = () => {
     return (
-        <View style={styles.container}>
-            <Text>OnBoardingItem</Text>
+        <View style={[styles.container, { width, backgroundColor:item.backgroundColor}]}>
+        <Image source={item.image} style={[styles.image, { width, resizeMode: 'contain' }]} />
+        <View style={{ flex: 0.3 }}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.subTitle}>{item.subTitle}</Text>
+            <Text style={styles.description}>{item.description}</Text>
         </View>
+    </View>
     );
 };
 
@@ -15,10 +20,32 @@ const OnBoardingItem = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+    
     },
+    image: {
+        flex: 0.7,
+    }, title: {
+        fontWeight: '800',
+        fontSize: 28,
+        marginBottom: 10,
+        color: COLORS.secondary,
+        paddingHorizontal: 20
+
+    },
+    subTitle: {
+        fontWeight: '600',
+        fontSize: 24,
+        marginBottom: 10,
+        color: COLORS.secondary,
+        paddingHorizontal: 20
+
+    },
+    description: {
+        fontWeight: '300',
+        fontSize:14,
+        color: COLORS.secondary, 
+        paddingHorizontal: 20
+    }
 });
 
 //make this component available to the app
